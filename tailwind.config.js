77741f5fad5f1d05034e85f7aa47ebbd8b470574
irling <italodavidramosling@gmail.com>
2025-04-05 23:52:44 -0500

    se agregaron los archivos para la creación de la web

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"], // Ajusta las rutas a tus archivos
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
